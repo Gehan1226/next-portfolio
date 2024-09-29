27cb4f550f1f49729313eeb676c23495e1ff833a
Gehan1226 <gehansithija2023@gmail.com>
2024-09-29 07:37:27 +0530

    implemented skill components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Hero from "./components/main/Hero";
+import Skills from "./components/main/Skills";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
-      <div className="flex flex-col h-[850px] gap-20">
-        <Hero />
-      </div>
-    </main>
+    <div className="flex flex-col gap-20">
+      <Hero />
+      <Skills />
+      
+    </div>
+  </main>
   );
 }
